3fd89c3a2dc3b1b9f7b27fe4e591c6fa7ecd3680
iqrasarfaraz1896-code <iqrasarfaraz1896@gmail.com>
2026-04-24 22:39:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+# script.js
+
+```javascript
+particlesJS("particles-js", {
+
+  particles: {
+
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+
+    color: {
+      value: "#ffffff"
+    },
+
+    shape: {
+      type: "circle"
+    },
+
+    opacity: {
+      value: 0.5
+    },
+
+    size: {
+      value: 3
+    },
+
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.2,
+      width: 1
+    },
+
+    move: {
+      enable: true,
+      speed: 2
+    }
+
+  },
+
+  interactivity: {
+
+    detect_on: "canvas",
+
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab"
+      },
+
+      resize: true
+    }
+
+  },
+
+  retina_detect: true
+
+});
+```
+
